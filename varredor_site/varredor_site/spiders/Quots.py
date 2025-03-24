9f23cba2cbd9160d61b7669a793f7040addb1d14
Dheividy Andrade <deividyandrade@hotmail.com>
2025-03-24 19:40:55 -0300

    up
    up

---
+++ varredor_site/varredor_site/spiders/Quots.py
@@ -0,0 +1,22 @@
+import scrapy
+# CanekCase
+class QuotesToScrapeSpider(scrapy.Spider):
+    
+    
+    #Identidade
+    name = 'frasebot'
+   
+    #Request
+    def start_requests(self):
+        # Definir url(s) a varrer
+        urls = ['https://quotes.toscrape.com/']
+
+        for url in urls:
+            yield scrapy.Request(url=url,callback=self.parse)
+
+
+    #Response
+    def parse(self, response):
+        # Aqui é onde você deve processar o que é retornado da response
+        with open('pagina.html', 'wb') as arquivo:
+            arquivo.write(response.body)
